54adc64d0d08c6084e322fcbd45298a5bb3deae3
liuxin <963875368@qq.com>
2020-11-23 23:14:50 +0800

    完善

---
+++ studyShell/studyShell.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#if  [] ;then fi    判断
+
+# yn="liuxin"
+# if [ "${yn}" == "liuxin" ] ; then
+#         echo "正确";
+# fi
+
+
+# yn1="a"
+# if [ "${yn1}"  == "b" ] ; then
+#      echo " ${yn1} "
+# else 
+#       echo  "不相同";
+# fi          
+
+
+# yn1="a"
+# if [ "${yn1}"  == "b" ] ; then
+#      echo " ${yn1} "
+# elif  [ "${yn1}" == "a" ]  ;then
+#      echo "elif"    
+# else 
+#       echo  "不相同";
+# fi        
