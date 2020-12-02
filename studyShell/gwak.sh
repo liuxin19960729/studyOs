41ad76d8e79bfd9e862f110d2aaa41491b5e9499
liuxin <963875368@qq.com>
2020-12-02 16:38:35 +0800

    完善

--- studyShell/gwak.sh
+++ studyShell/gwak.sh
@@ -4,3 +4,23 @@ gawk -F:  '{print $1}' /etc/passwd
 
 -F : 把 /etc/passwd 以:来作为分隔符 '{print $1}'  打印第一列
 $0 全部列数     $1....$n   第一列  第n列               
+
+
+
+echo "我.是.刘鑫" | gawk -F . '{$3="徐栎"; print $0 }'
+
+
+gawk 脚本建议使用.gawk 作为后缀
+
+xxx.gawk
+{
+    命令
+}
+
+gawk  -f   脚本路径  文件  
+
+
+
+
+
+
