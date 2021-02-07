639de3757e4ce2a1175e96e2bcd47a26550a0110
liuxin <963875368@qq.com>
2021-02-07 11:50:55 +0800

    完善

--- NodeJs/study1/src/index.ts
+++ NodeJs/study1/src/index.ts
@@ -1,11 +1,14 @@
-import async= require("async");
-import fs= require("fs");
-async.map(['file1','file2','file3'], async (item,cb)=>{
-   console.log(item);
-   cb&&cb(null,"xx"+item);
-}, function(err, results) {
-   console.log(results);
-});
+// import async= require("async");
+// import fs= require("fs");
+// async.map(['file1','file2','file3'], async (item,cb)=>{
+//    console.log(item);
+//    cb&&cb(null,"xx"+item);
+// }, function(err, results) {
+//    console.log(results);
+// });
+
+import { any } from "async";
+import { mainModule } from "process";
 
 
 // async.series([
@@ -18,3 +21,56 @@ async.map(['file1','file2','file3'], async (item,cb)=>{
 //         console.log("33")
 //      }
 // ]);
+
+class T1{
+   a=1;
+   aa(){
+      console.log("T1.aa")
+   }
+
+   bb(){
+      console.log("T1.aa")
+   }
+
+}
+
+class T2{
+   a=1;
+   aa(){
+      console.log("T1.aa")
+   }
+
+   bb(){
+      console.log("T1.aa")
+   }
+
+}
+
+
+
+
+function main(){
+  let result=<T1&T2>{};
+  let t1=new T1();
+  for(let id in t1){
+       (<any>result)[id]=(<any>t1)[id]
+  }
+  let t2=new T2();
+  for(let id in t2){
+   if(!result.hasOwnProperty(id)){
+      (<any>result)[id]=(<any>t1)[id]
+   }else{
+      console.log("result %s key has",id)
+   }
+ 
+}
+
+}
+
+
+
+main();
+
+
+
+
---
+++ 重学cocos/Ts高级类型.md
@@ -0,0 +1,106 @@
+1.获取对象key的方式
+```ts
+对象bean
+
+let keys=Object.keys(bean);
+
+//遍历对象的key
+for (key in bean){
+
+}
+
+```
+
+
+
+2.将两个属性融合(交叉类型)
+```ts
+ 只允许存入T1 和 T2 的同名属性和同名属性和同类型的值
+ let result=<T1&T2>{};
+
+
+例：
+
+  let result=<T1&T2>{};
+  let t1=new T1();
+  for(let id in t1){
+       (<any>result)[id]=(<any>t1)[id]
+  }
+  let t2=new T2();
+  for(let id in t2){
+   if(!result.hasOwnProperty(id)){
+      (<any>result)[id]=(<any>t1)[id]
+   }else{
+      console.log("result %s key has",id)
+   }
+
+```
+
+
+
+
+
+2.联合类型
+
+```ts
+function padLeft(value: string, padding: string | number) {
+    // ...
+}
+
+
+
+padding 允许使用string类型或number类型
+```
+
+
+
+3.类型保护与区分类型
+
+```
+typeof x === "number";  
+
+typeof x === "string";
+
+判断x 是否为number类型。
+判断 x是否为 string类型
+
+
+
+
+
+
+instanceof类型保护
+  通过对象的构造函数来细化类型的一种方式
+  instanceof的右侧要求是一个构造函数
+
+```
+
+
+
+
+类型别名
+
+
+```
+type Name = string;
+type NameResolver = () => string;
+type NameOrResolver = Name | NameResolver;
+function getName(n: NameOrResolver): Name {
+    if (typeof n === 'string') {
+        return n;
+    }
+    else {
+        return n();
+    }
+}
+```
+
+
+
+
+
+映射类型
+
+```
+
+```
